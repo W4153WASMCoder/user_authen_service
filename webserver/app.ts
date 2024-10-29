374643c357c0589940cbbc9f2933577e6ffb9624
FormerlyZeroCool <andrew.rubinstein93@gmail.com>
2024-10-28 23:43:48 -0400

    Add comments describing what to use for each of the backend services

--- webserver/app.ts
+++ webserver/app.ts
@@ -5,8 +5,10 @@ import type { Application } from 'express';
 import http, { Server as HttpServer } from 'http';
 import fs from 'fs';
 import { generate_routes } from './routes/router.js';
+// for projects microservice
 import project_router from './routes/project.js';
 import project_files_router from './routes/project_files.js';
+// for users microservice
 import users_router from './routes/users.js';
 import user_tokens_router from './routes/user_tokens.js';
 import swagger from './swagger.js';
@@ -28,8 +30,10 @@ app.use(express.static('../'));
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 generate_routes(app);
+// for projects microservice
 app.use('/projects', project_router);
 app.use('/project_files', project_files_router);
+// for users microservice
 app.use('/users', users_router);
 app.use('/user_tokens', user_tokens_router);
 swagger(app as Express);
