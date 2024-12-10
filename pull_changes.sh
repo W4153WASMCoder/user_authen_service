7f331baad1b4658eb65ad15dfeadbef5ce1929b1
FormerlyZeroCool <andrew.rubinstein93@gmail.com>
2024-12-10 02:07:57 -0500

    Update pull changes

--- pull_changes.sh
+++ pull_changes.sh
@@ -6,7 +6,7 @@ git fetch origin
 output=$(git pull origin main)
 
 if [[ $output != "Already up to date." ]]; then
-    echo "Files have changed. Rebuilding Docker image and restarting container..."
+    echo "Files have changed. Restarting container using a public Node.js image..."
 
     if docker ps | grep -q "myservice-container"; then
         echo "Stopping running container..."
@@ -15,14 +15,16 @@ if [[ $output != "Already up to date." ]]; then
         echo "Old container stopped and removed."
     fi
 
-    echo "Rebuilding Docker image with no cache..."
-    docker build --no-cache -t myservice-image .
-
     echo "Starting new Docker container..."
-    docker run -d --name myservice-container -p 8081:8081 myservice-image
+    docker run -d --name myservice-container \
+        -p 8081:8081 \
+        -w /home/admin/user \
+        -v /home/admin/user:/home/admin/user \
+        node:18-alpine \
+        sh -c "npm install && npm run build && npm start"
+
     echo "New container started."
 
 else
     echo "No changes detected. The Docker container will not be restarted."
 fi
-#
