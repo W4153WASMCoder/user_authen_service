7069a10a312eb016e53bcae6ab41f71fa3f4c7c1
FormerlyZeroCool <andrew.rubinstein93@gmail.com>
2024-12-10 01:56:59 -0500

    update pull changes

--- pull_changes.sh
+++ pull_changes.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Navigate to your project directory
 cd /home/admin/user/ || exit
 
-# Fetch and pull the latest changes
 git fetch origin
 output=$(git pull origin main)
 
-# Check if any files were updated by the pull command
 if [[ $output != "Already up to date." ]]; then
     echo "Files have changed. Rebuilding Docker image and restarting container..."
 
-    # Stop and remove the existing Docker container if it's running
     if docker ps | grep -q "myservice-container"; then
         echo "Stopping running container..."
         docker stop myservice-container
@@ -19,13 +15,11 @@ if [[ $output != "Already up to date." ]]; then
         echo "Old container stopped and removed."
     fi
 
-    # Rebuild the Docker image without cache
     echo "Rebuilding Docker image with no cache..."
     docker build --no-cache -t myservice-image .
 
-    # Start a new Docker container from the rebuilt image
     echo "Starting new Docker container..."
-    docker run -d --name myservice-container -p 3000:3000 myservice-image
+    docker run -d --name myservice-container -p 8081:8081 myservice-image
     echo "New container started."
 
 else
